e928a2c81b51ec17bf828b273e344b7b63ac12ef
zhuima <zhuima314@gmail.com>
2023-05-08 17:52:03 +0800

    update input content check

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
-import { useTranslations } from 'next-intl'
+import { useTranslations } from "next-intl";
 import { Toaster, toast } from "react-hot-toast";
 import DropDown, { FormType } from "../components/DropDown";
 import Footer from "../components/Footer";
@@ -15,65 +15,70 @@ import ResizablePanel from "../components/ResizablePanel";
 import { marked } from "marked";
 
 const Home: NextPage = () => {
-  const t = useTranslations('Index')
+  const t = useTranslations("Index");
 
   const [loading, setLoading] = useState(false);
   const [chat, setChat] = useState("");
   const [form, setForm] = useState<FormType>("paragraphForm");
-  const [api_key, setAPIKey] = useState("")
+  const [api_key, setAPIKey] = useState("");
   const [generatedChat, setGeneratedChat] = useState<String>("");
 
   console.log("Streamed response: ", generatedChat);
 
-  const prompt =
-    form === 'paragraphForm'?
-      `${chat}`
-      : `${chat}`;
+  const prompt = form === "paragraphForm" ? `${chat}` : `${chat}`;
 
-  const useUserKey = process.env.NEXT_PUBLIC_USE_USER_KEY === "true" ? true : false;
+  const useUserKey =
+    process.env.NEXT_PUBLIC_USE_USER_KEY === "true" ? true : false;
 
   const generateChat = async (e: any) => {
     e.preventDefault();
     setGeneratedChat("");
     setLoading(true);
-    if (useUserKey && api_key == ""){
-      toast.error(t("API_KEY_NULL_ERROR"))
-      setLoading(false)
-      return
+    if (useUserKey && api_key == "") {
+      toast.error(t("API_KEY_NULL_ERROR"));
+      setLoading(false);
+      return;
     }
-    if (chat == ""){
-      toast.error(t("CONTENT_NULL_ERROR"))
-      setLoading(false)
-      return
+    if (chat == "") {
+      toast.error(t("CONTENT_NULL_ERROR"));
+      setLoading(false);
+      return;
     }
-    const response = useUserKey ?
-      await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-          api_key,
-        }),
-      })
-    :
-      await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      })
+
+    const myArray = chat.split(" ");
+    if (myArray[1] > 4) {
+      toast.error(t("INVILID_CONTENT_ERROR"));
+      setLoading(false);
+      return;
+    }
+
+    const response = useUserKey
+      ? await fetch("/api/generate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+            api_key,
+          }),
+        })
+      : await fetch("/api/generate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        });
 
     console.log("Edge function returned.");
 
     if (!response.ok) {
-      toast.error("服务繁忙，请稍后再试")
+      toast.error("服务繁忙，请稍后再试");
       setLoading(false);
-      return
+      return;
     }
 
     // This data is a ReadableStream
@@ -99,44 +104,44 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>{t('title')}</title>
+        <title>{t("title")}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        
-
-
-
-
-
         <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900">
-          {t('description1')} <br></br><div className=" px-4 py-2 sm:mt-3 mt-8  w-full"></div>{t('description2')}
+          {t("description1")} <br></br>
+          <div className=" px-4 py-2 sm:mt-3 mt-8  w-full"></div>
+          {t("description2")}
         </h1>
-        <p className="text-slate-500 mt-5">{t('slogan')}</p>
-
+        <p className="text-slate-500 mt-5">{t("slogan")}</p>
 
         <div className="max-w-xl w-full">
-        { useUserKey &&(
+          {useUserKey && (
             <>
               <div className="flex mt-10 items-center space-x-3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#000" d="M7 14q-.825 0-1.412-.588Q5 12.825 5 12t.588-1.413Q6.175 10 7 10t1.412.587Q9 11.175 9 12q0 .825-.588 1.412Q7.825 14 7 14Zm0 4q-2.5 0-4.25-1.75T1 12q0-2.5 1.75-4.25T7 6q1.675 0 3.038.825Q11.4 7.65 12.2 9H21l3 3l-4.5 4.5l-2-1.5l-2 1.5l-2.125-1.5H12.2q-.8 1.35-2.162 2.175Q8.675 18 7 18Zm0-2q1.4 0 2.463-.85q1.062-.85 1.412-2.15H14l1.45 1.025L17.5 12.5l1.775 1.375L21.15 12l-1-1h-9.275q-.35-1.3-1.412-2.15Q8.4 8 7 8Q5.35 8 4.175 9.175Q3 10.35 3 12q0 1.65 1.175 2.825Q5.35 16 7 16Z"/></svg>
-                <p className="text-left font-medium">
-                  {t('step0')}{" "}
-
-                </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#000"
+                    d="M7 14q-.825 0-1.412-.588Q5 12.825 5 12t.588-1.413Q6.175 10 7 10t1.412.587Q9 11.175 9 12q0 .825-.588 1.412Q7.825 14 7 14Zm0 4q-2.5 0-4.25-1.75T1 12q0-2.5 1.75-4.25T7 6q1.675 0 3.038.825Q11.4 7.65 12.2 9H21l3 3l-4.5 4.5l-2-1.5l-2 1.5l-2.125-1.5H12.2q-.8 1.35-2.162 2.175Q8.675 18 7 18Zm0-2q1.4 0 2.463-.85q1.062-.85 1.412-2.15H14l1.45 1.025L17.5 12.5l1.775 1.375L21.15 12l-1-1h-9.275q-.35-1.3-1.412-2.15Q8.4 8 7 8Q5.35 8 4.175 9.175Q3 10.35 3 12q0 1.65 1.175 2.825Q5.35 16 7 16Z"
+                  />
+                </svg>
+                <p className="text-left font-medium">{t("step0")} </p>
               </div>
               <input
-                  value={api_key}
-                  onChange={(e) => setAPIKey(e.target.value)}
-                  className="w-full rounded-md border-2 border-gray-300 shadow-sm focus:border-black focus:ring-black p-2"
-                  placeholder={
-                    t('openaiApiKeyPlaceholder')
-                  }
-                />
-            </>)
-          }
+                value={api_key}
+                onChange={(e) => setAPIKey(e.target.value)}
+                className="w-full rounded-md border-2 border-gray-300 shadow-sm focus:border-black focus:ring-black p-2"
+                placeholder={t("openaiApiKeyPlaceholder")}
+              />
+            </>
+          )}
 
           <div className="flex mt-10 items-center space-x-3">
             <Image
@@ -146,9 +151,7 @@ const Home: NextPage = () => {
               alt="1 icon"
               className="mb-5 xs:mb-0"
             />
-            <p className="text-left font-medium">
-              {t('step1')}{" "}
-            </p>
+            <p className="text-left font-medium">{t("step1")} </p>
           </div>
 
           <textarea
@@ -156,9 +159,7 @@ const Home: NextPage = () => {
             onChange={(e) => setChat(e.target.value)}
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-2"
-            placeholder={
-              t('placeholder')
-            }
+            placeholder={t("placeholder")}
           />
 
           {!loading && (
@@ -166,7 +167,7 @@ const Home: NextPage = () => {
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-5 mt-8 hover:bg-black/80 w-full"
               onClick={(e) => generateChat(e)}
             >
-              {t('simplifierButton')} &rarr;
+              {t("simplifierButton")} &rarr;
             </button>
           )}
           {loading && (
@@ -181,19 +182,20 @@ const Home: NextPage = () => {
           <br></br>
           <div className="mt-1 items-center space-x-3">
             <span className="text-slate-200">
-                {t('privacyPolicy1')}
+              {t("privacyPolicy1")}
               <a
                 className="text-blue-200 hover:text-blue-400"
                 href="https://github.com/guaguaguaxia/weekly_report/blob/main/privacy.md"
                 target="_blank"
                 rel="noopener noreferrer"
-              >{' '}{t('privacyPolicy2')}</a>
+              >
+                {" "}
+                {t("privacyPolicy2")}
+              </a>
             </span>
             <br></br>
-            
 
-
-            <br/>
+            <br />
           </div>
         </div>
         <Toaster
@@ -209,7 +211,7 @@ const Home: NextPage = () => {
                 <>
                   <div>
                     <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
-                      {t('simplifiedContent')}
+                      {t("simplifiedContent")}
                     </h2>
                   </div>
                   <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
@@ -229,7 +231,7 @@ const Home: NextPage = () => {
                           __html: marked(generatedChat.toString(), {
                             gfm: true,
                             breaks: true,
-                            smartypants: true
+                            smartypants: true,
                           }),
                         }}
                       ></p>
@@ -255,5 +257,5 @@ export function getStaticProps({ locale }: { locale: string }) {
         ...require(`../messages/${locale}.json`),
       },
     },
-  }
+  };
 }
